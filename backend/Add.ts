c72347036ccfa21b54c91c85b446b7a357c80ff7
Jose Valerio <contact@joswayski.com>
2021-04-28 21:01:11 -0400

    Installed jest, added gitignore

---
+++ backend/Add.ts
@@ -0,0 +1,7 @@
+/**
+ * @description Adds two numbers together
+ * @param a Any number
+ * @param b Any number
+ * @returns Returns the sum of `a` + `b`
+ */
+const sum = (a: number, b: number): number => a + b;
