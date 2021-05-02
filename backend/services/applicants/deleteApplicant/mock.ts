079f41278be11bf73623f98a42f62eab6fde9b01
Jose Valerio <contact@joswayski.com>
2021-05-02 03:38:34 -0400

    minor spacing issues

--- backend/services/applicants/deleteApplicant/mock.ts
+++ backend/services/applicants/deleteApplicant/mock.ts
@@ -8,4 +8,4 @@ console.log(deleteApplicant("11122233341112223334"));
 console.log("\nFailures:");
 const failStates = ["", "ad2f2", 423423, null];
 
-failStates.forEach((state) => console.log(deleteApplicant(state)));
+failStates.forEach((state: any) => console.log(deleteApplicant(state)));
--- backend/services/applicants/getApplicant/getApplicant.js
+++ backend/services/applicants/getApplicant/getApplicant.js
@@ -4,37 +4,36 @@ exports.getApplicant = void 0;
 var Joi = require("joi");
 var idLength = 20;
 var getApplicant = function (id) {
-    if (!id)
-        return { message: "ERROR: 'id' is required" };
-    var validation = Joi.string()
-        .required()
-        .length(idLength)
-        .validate(id, {
-        abortEarly: false,
-        errors: {
-            wrap: {
-                label: "''",
-            },
+  if (!id) return { message: "ERROR: 'id' is required" };
+  var validation = Joi.string()
+    .required()
+    .length(idLength)
+    .validate(id, {
+      abortEarly: false,
+      errors: {
+        wrap: {
+          label: "''",
         },
+      },
     });
-    if (validation.error) {
-        return {
-            message: "ERROR: " + validation.error.message,
-        };
-    }
+  if (validation.error) {
     return {
-        message: "Applicant retrieved succesfully!",
-        applicant: {
-            email: "cowabunga@nickatnite.com",
-            first_name: "James",
-            last_name: "Jameson",
-            id: id,
-            phone_number: "8391823452",
-            funnel: "Software Engineer",
-            stage: "Document Upload",
-            location: ["Remote", "NYC"],
-            created_at: new Date().toISOString(),
-        },
+      message: "ERROR: " + validation.error.message,
     };
+  }
+  return {
+    message: "Applicant retrieved succesfully!",
+    applicant: {
+      email: "cowabunga@nickatnite.com",
+      first_name: "James",
+      last_name: "Jameson",
+      id: id,
+      phone_number: "8391823452",
+      funnel: "Software Engineer",
+      stage: "Document Upload",
+      location: ["Remote", "NYC"],
+      created_at: new Date().toISOString(),
+    },
+  };
 };
 exports.getApplicant = getApplicant;
