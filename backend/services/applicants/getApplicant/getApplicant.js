f0e8e6d894d5c4572bf1e6ebfbaa2ab886d4c796
Jose Valerio <contact@joswayski.com>
2021-05-02 14:46:26 -0400

    Simple space missing, forgot to save last time

--- backend/services/applicants/getApplicant/getApplicant.js
+++ backend/services/applicants/getApplicant/getApplicant.js
@@ -4,36 +4,37 @@ exports.getApplicant = void 0;
 var Joi = require("joi");
 var idLength = 20;
 var getApplicant = function (id) {
-  if (!id) return { message: "ERROR: 'id' is required" };
-  var validation = Joi.string()
-    .required()
-    .length(idLength)
-    .validate(id, {
-      abortEarly: false,
-      errors: {
-        wrap: {
-          label: "''",
+    if (!id)
+        return { message: "ERROR: 'id' is required" };
+    var validation = Joi.string()
+        .required()
+        .length(idLength)
+        .validate(id, {
+        abortEarly: false,
+        errors: {
+            wrap: {
+                label: "''",
+            },
         },
-      },
     });
-  if (validation.error) {
+    if (validation.error) {
+        return {
+            message: "ERROR: " + validation.error.message,
+        };
+    }
     return {
-      message: "ERROR: " + validation.error.message,
+        message: "Applicant retrieved succesfully!",
+        applicant: {
+            email: "cowabunga@nickatnite.com",
+            first_name: "James",
+            last_name: "Jameson",
+            id: id,
+            phone_number: "8391823452",
+            funnel: "Software Engineer",
+            stage: "Document Upload",
+            location: ["Remote", "NYC"],
+            created_at: new Date().toISOString(),
+        },
     };
-  }
-  return {
-    message: "Applicant retrieved succesfully!",
-    applicant: {
-      email: "cowabunga@nickatnite.com",
-      first_name: "James",
-      last_name: "Jameson",
-      id: id,
-      phone_number: "8391823452",
-      funnel: "Software Engineer",
-      stage: "Document Upload",
-      location: ["Remote", "NYC"],
-      created_at: new Date().toISOString(),
-    },
-  };
 };
 exports.getApplicant = getApplicant;
