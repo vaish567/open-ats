49999c9901a4c63a166f5878d92d73fc61350d43
Jose Valerio <contact@joswayski.com>
2021-05-03 00:25:47 -0400

    Added basic checking to see if a stage exists

---
+++ backend/utils/doesStageExist/doesStageExist.ts
@@ -0,0 +1,21 @@
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
+const dynamodb = new DynamoDB({ apiVersion: "2012-08-10" });
+
+/**
+ * @description Checks if a stage exists inside of a funnel
+ * @param funnelId - The id of the funnel that you want to check
+ * @param stageName - The stage name to check for (Questionnaire, Set Up Profile, etc.)
+ */
+const doesStageExist = async (funnelId: string, stageName: string) => {
+  const params = {
+    TableName: "OpenATS", // TODO switch to parameter store?
+    Key: {
+      PK: { S: funnelId },
+      SK: { S: `STAGE_TITLE#${stageName}` },
+    },
+  };
+
+  const response = await dynamodb.getItem(params);
+  return response.Item ? true : false;
+};
+export default doesStageExist;
